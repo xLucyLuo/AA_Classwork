af211b77dd9a0b1e3f296159aaf90560f3643470
Lucy Luo <Your Email>
2022-04-26 09:46:59 -0700

    W2D2

--- W2D2/hotel_project/lib/hotel.rb
+++ W2D2/hotel_project/lib/hotel.rb
@@ -1,5 +1,37 @@
 require_relative "room"
 
 class Hotel
-  
+  def initialize(name, rooms)
+    @name = name
+    @rooms = {}
+    rooms.each{|name, capacity| @rooms[name] = Room.new(capacity)}
+  end
+
+  def name
+    @name.split(" ").map{|word| word.capitalize}.join(" ")
+  end
+
+  attr_reader :rooms
+
+  def room_exists?(name)
+    rooms.include?(name)
+  end
+
+  def check_in(person, room)
+    if !room_exists?(room)
+        p "sorry, room does not exist"
+    elsif @rooms[room].add_occupant(person)
+        p 'successful'
+    else
+        p "sorry, room is full"
+    end
+  end
+
+  def has_vacancy?
+    @rooms.each_value.any?{|room| !room.full?}
+  end
+
+  def list_rooms
+    @rooms.each{|name,room| p name + ".*" + room.available_space.to_s}
+  end
 end
--- W2D2/hotel_project/lib/room.rb
+++ W2D2/hotel_project/lib/room.rb
@@ -1,3 +1,33 @@
 class Room
-  
+    def initialize(capacity)
+        @capacity = capacity
+        @occupants = []
+    end
+
+    attr_reader :capacity, :occupants
+
+    def full?
+        occupants.length >= capacity
+    end
+
+    def available_space
+        capacity - occupants.length
+    end
+
+    def add_occupant(person)
+        if !full?
+            @occupants << person
+            return true 
+        end 
+        false
+
+        if !full?
+            @occupants << person
+            return true 
+        end
+
+        false
+    end
+
+
 end
