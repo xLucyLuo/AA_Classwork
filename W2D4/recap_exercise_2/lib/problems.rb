d3feca25e0b9505d5b3e53c84a9e38a0ee537a7a
Lucy Luo <Your Email>
2022-04-27 15:46:38 -0700

    w2d4

--- W2D4/recap_exercise_2/lib/problems.rb
+++ W2D4/recap_exercise_2/lib/problems.rb
@@ -1,29 +1,51 @@
+require "byebug"
 # Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple
 # of both of the given numbers
 def least_common_multiple(num_1, num_2)
+    #num_1.lcm(num_2)
 
+    max = [num_1, num_2].max
+    min = [num_1,num_2].min
+    multiple = 1
+
+    until false
+        return max * multiple if (max * multiple) % min == 0
+        multiple+=1
+    end
 end
 
 
 # Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
 # most in the string.
 def most_frequent_bigram(str)
+    count_bigrams = Hash.new(0)
 
+    (0...str.length-1).each do |i|
+        count_bigrams[str[i..i+1]]+=1
+    end
+    count_bigrams.to_a.sort_by{|el| el[1]}.last.first
 end
-
 
 class Hash
     # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
     def inverse
-
+        inverse = {}
+        self.each{|k,v| inverse[v]=k}
+        inverse
     end
 end
-
 
 class Array
     # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
     def pair_sum_count(num)
+        pair_count = 0
 
+        (0...self.length-1).each do |i|
+            (i+1...self.length).each do |j|
+                pair_count+=1 if self[i]+self[j] == num
+            end
+        end
+        pair_count
     end
 
     # Write a method, Array#bubble_sort, that takes in an optional proc argument.
@@ -40,6 +62,22 @@ class Array
     #
     # This should remind you of the spaceship operator! Convenient :)
     def bubble_sort(&prc)
+        prc||=Proc.new{|a,b| a<=>b}
 
+        sorted = false
+
+        until sorted
+            sorted = true
+            (0...self.length-1).each do |i| 
+                compare = prc.call(self[i],self[i+1])
+                if compare == 1
+                    self[i], self[i+1] = self[i+1], self[i]
+                    sorted = false
+                end
+            end 
+            #debugger
+        end
+
+        self
     end
 end
