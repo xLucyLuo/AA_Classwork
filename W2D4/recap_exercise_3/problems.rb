832cdc0cfc9889e4fbee2cbabd958f5ac9760f72
Lucy Luo <Your Email>
2022-04-28 10:07:40 -0700

    W2D4

--- W2D4/recap_exercise_3/problems.rb
+++ W2D4/recap_exercise_3/problems.rb
@@ -217,11 +217,11 @@ def prime_factorization(num)
     end
 end
 
-# Examples
-p prime_factorization(12)     == [2, 2, 3]
-p prime_factorization(24)     == [2, 2, 2, 3]
-p prime_factorization(25)     == [5, 5]
-p prime_factorization(60)     == [2, 2, 3, 5]
-p prime_factorization(7)      == [7]
-p prime_factorization(11)     == [11]
-p prime_factorization(2017)   == [2017]
+# # Examples
+# p prime_factorization(12)     == [2, 2, 3]
+# p prime_factorization(24)     == [2, 2, 2, 3]
+# p prime_factorization(25)     == [5, 5]
+# p prime_factorization(60)     == [2, 2, 3, 5]
+# p prime_factorization(7)      == [7]
+# p prime_factorization(11)     == [11]
+# p prime_factorization(2017)   == [2017]
